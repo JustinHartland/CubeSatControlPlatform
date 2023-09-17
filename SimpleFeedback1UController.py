daa93067c7838886a7c3dd5340baa4eb1c4f84eb
Justin Hartland <justinhartland2@gmail.com>
2023-09-17 14:54:41 -0400

    Update

--- SimpleFeedback1UController.py
+++ SimpleFeedback1UController.py
@@ -2,6 +2,7 @@ import time
 import board
 import busio
 import adafruit_lsm9ds1
+import math
 
 # Initialize I2C bus
 i2c = busio.I2C(board.SCL, board.SDA)
@@ -16,9 +17,15 @@ while True:
     mag_x, mag_y, mag_z = sensor.magnetic
 
     # Calculate Euler angles
-    roll = 180 * (accel_x / 9.81)  # Convert acceleration to degrees
-    pitch = 180 * (accel_y / 9.81)  # Convert acceleration to degrees
-    yaw = 180 * (gyro_z / 32767.0)  # Convert gyroscope data to degrees
+    roll = math.atan2(gyro_y, gyro_z)
+
+    gyro_z2 = gyro_y * math.sin(roll) + gyro_z * math.cos(roll)
+    pitch = math.atan(-1 * gyro_x / gyro_z2)
+
+    mag_y2 = mag_z * math.sin(roll) - mag_y * math.cos(roll)
+    mag_z2 = mag_y * math.sin(roll) + mag_z * math.cos(roll)
+    mag_x3 = mag_x * math.cos(pitch) + mag_z2 * math.sin(pitch)
+    yaw = math.atan2(mag_y2, mag_x3)
 
     # Print the Euler angles
     print("\nRoll: {:.2f} degrees".format(roll))
