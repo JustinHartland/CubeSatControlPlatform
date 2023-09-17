aa2f9df090ea3cd038c9991df28a7244e31de264
Justin Hartland <justinhartland2@gmail.com>
2023-09-17 15:56:53 -0400

    Update

--- SimpleFeedback1UController.py
+++ SimpleFeedback1UController.py
@@ -2,10 +2,9 @@ import math
 import time
 import board
 import busio
-import scipy
-import numpy
 from adafruit_lsm9ds1 import LSM9DS1
-from pyquaternion import Quaternion
+import numpy as np
+from scipy.spatial.transform import Rotation as R
 
 # Initialize I2C bus
 i2c = busio.I2C(board.SCL, board.SDA)
@@ -23,18 +22,11 @@ while True:
     pitch = math.degrees(math.atan2(accel_x, math.sqrt(accel_y**2 + accel_z**2)))
     roll = math.degrees(math.atan2(accel_y, math.sqrt(accel_x**2 + accel_z**2)))
 
-    # Create a quaternion using the calculated pitch and roll
-    quaternion_data = Quaternion(axis=[1, 0, 0], angle=math.radians(roll)) * \
-                     Quaternion(axis=[0, 1, 0], angle=math.radians(pitch))
+    # Create a rotation matrix using the calculated pitch and roll
+    rotation_matrix = R.from_euler('zyx', [yaw, pitch, roll], degrees=True).as_matrix()
 
-    # Calculate yaw angle using magnetometer data (if needed)
-    # Replace this part if you have a specific yaw calculation method
-
-    # Convert quaternion to Euler angles (roll, pitch, yaw)
-    euler_angles = quaternion_data.to_euler_angles(degrees=True)
-
-    # Extract Euler angles
-    roll, pitch, yaw = euler_angles
+    # Extract Euler angles from the rotation matrix
+    yaw, pitch, roll = R.from_matrix(rotation_matrix).as_euler('zyx', degrees=True)
 
     # Print the Euler angles
     print("Roll: {:.2f} degrees".format(roll))
@@ -42,4 +34,4 @@ while True:
     print("Yaw: {:.2f} degrees".format(yaw))
 
     # Delay for a short time before the next reading
-    time.sleep(0.1)
+    time.sleep(0.5)
