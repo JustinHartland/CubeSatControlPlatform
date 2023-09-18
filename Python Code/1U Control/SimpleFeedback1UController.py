922e0db025d16b7225e27c55ab1c66557e90799e
Justin Hartland <justinhartland2@gmail.com>
2023-09-18 10:15:49 -0400

    Added madgwick filter

--- Python Code/1U Control/SimpleFeedback1UController.py
+++ Python Code/1U Control/SimpleFeedback1UController.py
@@ -1,38 +1,32 @@
-import math
 import time
 import board
 import busio
 import adafruit_lsm9ds1
+from ahrs.filters import Madgwick
 import numpy as np
-from scipy.spatial.transform import Rotation as R
 
-# Initialize I2C bus
+# Setup I2C connection
 i2c = board.I2C()
-
-# Initialize LSM9DS1 sensor
 sensor = adafruit_lsm9ds1.LSM9DS1_I2C(i2c)
 
+# Create the Madgwick filter object
+madgwick = Madgwick()
+
 while True:
-    # Read accelerometer, gyroscope, and magnetometer data
-    accel_x, accel_y, accel_z = sensor.acceleration
-    gyro_x, gyro_y, gyro_z = sensor.gyro
-    mag_x, mag_y, mag_z = sensor.magnetic
+    # Get accelerometer, gyroscope, and magnetometer data
+    ax, ay, az = sensor.acceleration
+    gx, gy, gz = sensor.gyro
+    mx, my, mz = sensor.magnetic
 
-    # Calculate pitch and roll angles using accelerometer data
-    pitch = math.degrees(math.atan2(accel_x, math.sqrt(accel_y**2 + accel_z**2)))
-    roll = math.degrees(math.atan2(accel_y, math.sqrt(accel_x**2 + accel_z**2)))
-    yaw = math.degrees(math.atan2(mag_y, mag_x))
+    # Update the filter with the new data
+    madgwick.updateIMU(np.array([gx, gy, gz]), np.array([ax, ay, az]))
 
-    # Create a rotation matrix using the calculated pitch and roll
-    rotation_matrix = R.from_euler('zyx', [yaw, pitch, roll], degrees=True).as_matrix()
+    # The orientation is directly available as a quaternion
+    q = madgwick.quaternion
 
-    # Extract Euler angles from the rotation matrix
-    yaw, pitch, roll = R.from_matrix(rotation_matrix).as_euler('zyx', degrees=True)
+    # Convert quaternion to Euler angles
+    euler = q.to_euler(degrees=True)  # Convert to Euler angles in degrees
 
-    # Print the Euler angles
-    print("Roll: {:.2f} degrees".format(roll))
-    print("Pitch: {:.2f} degrees".format(pitch))
-    print("Yaw: {:.2f} degrees".format(yaw))
+    print(f"Roll: {euler[0]:.2f}, Pitch: {euler[1]:.2f}, Yaw: {euler[2]:.2f}")
 
-    # Delay for a short time before the next reading
-    time.sleep(0.5)
+    time.sleep(0.01)  # Adjust as needed
