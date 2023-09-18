1c361d7c39d01b3c47f3bdf54352b859e8069898
Justin Hartland <justinhartland2@gmail.com>
2023-09-18 10:35:18 -0400

    Update

--- Python Code/1U Control/SimpleFeedback1UController.py
+++ Python Code/1U Control/SimpleFeedback1UController.py
@@ -12,14 +12,23 @@ sensor = adafruit_lsm9ds1.LSM9DS1_I2C(i2c)
 # Create the Madgwick filter object
 madgwick = Madgwick()
 
+#Allocation for quaternions
+Q = np.zeros((1, 4))
+t = 1
+
+#Initial attitude quaternion
+Q[0] = [1, 0, 0, 0]
+
 while True:
+    t += 1
+
     # Get accelerometer, gyroscope, and magnetometer data
     ax, ay, az = sensor.acceleration
     gx, gy, gz = sensor.gyro
     mx, my, mz = sensor.magnetic
 
     # Update the filter with the new data
-    madgwick.updateIMU(np.array([gx, gy, gz]), np.array([ax, ay, az]))
+    madgwick = updateIMU(Q[t - 1], np.array([gx, gy, gz]), np.array([ax, ay, az]), np.array([mx, my, mz]))
 
     # The orientation is directly available as a quaternion
     q = madgwick.quaternion
