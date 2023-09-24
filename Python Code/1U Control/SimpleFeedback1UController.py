859cc6911b6f4f262599e36284b594432cf3dbcf
Justin Hartland <justinhartland2@gmail.com>
2023-09-24 16:05:36 -0400

    Update

--- Python Code/1U Control/SimpleFeedback1UController.py
+++ Python Code/1U Control/SimpleFeedback1UController.py
@@ -8,6 +8,13 @@ from odrive.enums import *
 # Find a connected ODrive (this will block until you connect one)
 print("finding an odrive...")
 my_drive = odrive.find_any()
+# Calibrate motor and wait for it to finish
+print("starting calibration...")
+my_drive.axis0.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
+while my_drive.axis0.current_state != AXIS_STATE_IDLE:
+    time.sleep(0.1)
+
+my_drive.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
 my_drive.axis0.controller.config.control_mode = CONTROL_MODE_VELOCITY_CONTROL
 
 # Initialize I2C bus and sensor
