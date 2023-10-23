60b65b0973b230e8f4c96f487e25c04bbe2caf0f
Justin Hartland <justinhartland2@gmail.com>
2023-10-23 18:47:52 -0400

    Overhaul on IMU script

--- Inverted Pendulum/InertialMeasurementUnit.py
+++ Inverted Pendulum/InertialMeasurementUnit.py
@@ -8,9 +8,13 @@ class InertialMeasurementUnit:
     def __init__(self):
         i2c = board.I2C()
         self.lsm = adafruit_lsm9ds1.LSM9DS1_I2C(i2c)
+        self.accel_range = adafruit_lsm9ds1.ACCELRANGE_2G
+        self.gyro_scale = adafruit_lsm9ds1.GYROSCALE_245DPS
 
-        # Previous time for integration
-        self.prev_time = time.time()
+        #Euler angles
+        self.angle_x = 0
+        self.angle_y = 0
+        self.angle_z = 0
 
         # Previous angles from gyroscope
         self.gyro_angle_x = 0
@@ -22,14 +26,25 @@ class InertialMeasurementUnit:
         self.gyro_offset_y = 0
         self.gyro_offset_z = 0
 
+        # Gyro frequency
+        self.imu_rate_time = time.time()
+        self.imu_read_count = 0
+        self.imu_read_rate = 0
+        self.running = True
+
         # Filter coefficient for complementary filter
         self.alpha = 0.98
+
+        #Calibrate gyro
+        self.calibrate_gyro()
+
+        self.prev_time = time.time()
 
     def get_accel_angle(self):
         ax, ay, az = self.lsm.acceleration
         # Calculate roll and pitch angles from accelerometer data
-        accel_angle_x = math.atan2(ay, math.sqrt(ax**2 + az**2)) * 180 / math.pi
-        accel_angle_y = math.atan2(-ax, math.sqrt(ay**2 + az**2)) * 180 / math.pi
+        accel_angle_x = math.atan2(ay, az) * 180 / math.pi
+        accel_angle_y = math.atan2(ax, math.sqrt(ay**2 + az**2)) * 180 / math.pi
         return accel_angle_x, accel_angle_y
 
     def get_gyro_angle(self):
@@ -41,12 +56,12 @@ class InertialMeasurementUnit:
         curr_time = time.time()
         dt = curr_time - self.prev_time
         self.prev_time = curr_time
-        self.gyro_angle_x += gx * dt
-        self.gyro_angle_y += gy * dt
-        self.gyro_angle_z += gz * dt
+        self.gyro_angle_x = self.angle_x + gx * dt
+        self.gyro_angle_y = self.angle_y + gy * dt
+        self.gyro_angle_z = self.angle_z + gz * dt
         return self.gyro_angle_x, self.gyro_angle_y, self.gyro_angle_z
     
-    def calibrate_gyro(self, samples=500):
+    def calibrate_gyro(self, samples=1000):
         print("Calibrating gyro. Please keep the sensor stationary...")
         
         sum_gx = 0
@@ -72,21 +87,21 @@ class InertialMeasurementUnit:
         return self.alpha * (gyro_angle) + (1.0 - self.alpha) * accel_angle
 
     def get_euler_angles(self):
+        #Get raw accel and gyro data from IMU
         accel_angle_x, accel_angle_y = self.get_accel_angle()
         gyro_angle_x, gyro_angle_y, gyro_angle_z = self.get_gyro_angle()
 
         # Apply complementary filter
-        roll = self.complementary_filter(accel_angle_x, gyro_angle_x)
-        pitch = self.complementary_filter(accel_angle_y, gyro_angle_y)
-        yaw = gyro_angle_z  # Yaw is only from gyro as magnetometer is not used here
+        self.angle_x = self.complementary_filter(accel_angle_x, gyro_angle_x)
+        self.angle_y = self.complementary_filter(accel_angle_y, gyro_angle_y)
+        self.angle_z = gyro_angle_z  # Yaw is only from gyro as magnetometer is not used here
 
-        return roll, pitch, yaw
+        return self.angle_x, self.angle_y, self.angle_z
 
 #Example implementation code
 if __name__ == "__main__":
     IMU1 = InertialMeasurementUnit()
-    IMU1.calibrate_gyro()
     while True:
-        roll, pitch, yaw = IMU1.get_euler_angles()
-        print(f"Roll: {roll*180/math.pi:.2f}, Pitch: {pitch*180/math.pi:.2f}, Yaw: {yaw*180/math.pi:.2f}")
+        angle_x, angle_y, angle_z = IMU1.get_euler_angles()
+        print(f"Roll: {angle_x:.2f}, Pitch: {angle_y:.2f}, Yaw: {angle_z:.2f}")
         time.sleep(0.1)
