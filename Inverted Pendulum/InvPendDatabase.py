2888cb82c1c2a3aa10080f0e581a7e6e9ae59fcf
Justin Hartland <justinhartland2@gmail.com>
2023-10-30 18:00:31 -0400

    Updated name of database

--- Inverted Pendulum/InvPendDatabase.py
+++ Inverted Pendulum/InvPendDatabase.py
@@ -50,7 +50,7 @@ class InvPendDatabase:
         """
             Add a new trial to the trials table
         """
-        sql = ''' INSERT INTO trials DEFAULT VALUES '''
+        sql = ''' INSERT INTO imuDatabase DEFAULT VALUES '''
         cur = self.conn.cursor()
         cur.execute(sql)
         self.conn.commit()
