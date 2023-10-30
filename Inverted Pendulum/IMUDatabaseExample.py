7fa9a1c019dc849bc3c9ac14d5471d2b27da2971
Justin Hartland <justinhartland2@gmail.com>
2023-10-30 18:53:16 -0400

    Added plot functionality

--- Inverted Pendulum/IMUDatabaseExample.py
+++ Inverted Pendulum/IMUDatabaseExample.py
@@ -1,6 +1,35 @@
 from InertialMeasurementUnit import InertialMeasurementUnit
 from InvPendDatabase import InvPendDatabase
+import matplotlib.pyplot as plt
+import sqlite3
 import time
+
+def fetch_imu_data_for_trial(database_name, trial_id):
+    conn = sqlite3.connect(database_name)
+    cur = conn.cursor()
+    
+    cur.execute("SELECT time, angle_x FROM imu_data WHERE trial_id=?", (trial_id,))
+    data = cur.fetchall()
+    
+    conn.close()
+    
+    return data
+
+def plot_imu_data(data):
+    # Splitting data into columns for plotting
+    times, angles_x = zip(*data)
+    
+    plt.figure(figsize=(10, 6))
+
+    plt.plot(times, angles_x, label='Angle X')
+
+
+    plt.title("IMU Angles over Time")
+    plt.xlabel("Time")
+    plt.ylabel("Angle (degrees)")
+    plt.legend()
+
+    plt.show()
 
 #Create instance of InertialMeasurementUnit:
 imuDatabase = InvPendDatabase("InvPendDatabase.db")
@@ -18,7 +47,8 @@ imuData = []
 
 initialTime = time.time()
 
-while True:
+#Read in IMU data for 10 seconds
+while time.time() - initialTime < 10:
     IMU1.get_euler_angles()
     imuData = [initialTime-time.time(), *IMU1.rawAccelArray, *IMU1.rawGyroArray, IMU1.angle_x, IMU1.angle_y, IMU1.angle_z]
     imuDatabase.add_imu_data(trial_id, imuData)
@@ -26,3 +56,7 @@ while True:
     print(f"Angle X: {IMU1.angle_x} deg")
 
     time.sleep(0.01)
+
+#Plot data
+dataToPlot = fetch_imu_data_for_trial("InvPendDatabase.db", trial_id)
+plot_imu_data(dataToPlot)
