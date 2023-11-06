d0b4321c1f1171c0bb178eee84242b5c2e618b5f
Justin Hartland <justinhartland2@gmail.com>
2023-11-06 16:48:12 -0500

    Adding database again

--- invertedPendulumV2/main.py
+++ invertedPendulumV2/main.py
@@ -64,13 +64,13 @@ odrive_error_detected = False
 read_angle_thread = threading.Thread(target=pid.read_angle_thread, args=(IMU1, running, ))
 set_motor_torque_thread = threading.Thread(target=pid.set_torque_thread, args=(IMU1, node_id, bus, running))
 #print_thread = threading.Thread(target=pid.get_pos_vel_thread, args=(IMU1, node_id, bus, running, ))
-#add_data_to_database = threading.Thread(target=pid.add_data_to_database, args=(IMU1, invPendPIDDatabase, initialTime, trial_id, running, ))
+add_data_to_database = threading.Thread(target=pid.add_data_to_database, args=(IMU1, invPendPIDDatabase, initialTime, trial_id, running, ))
 
 #Initiate threads
 read_angle_thread.start()
 set_motor_torque_thread.start()
 #print_thread.start()
-#add_data_to_database.start()
+add_data_to_database.start()
 
 #Shutdown can bus upon ctrl+c
 try:
@@ -88,7 +88,7 @@ finally:
     read_angle_thread.join()
     set_motor_torque_thread.join()
     #print_thread.join()
-    #add_data_to_database.join()
+    add_data_to_database.join()
 
     bus.send(can.Message(arbitration_id=(node_id << 5 | 0x0E), data=struct.pack('<f', 0.0), is_extended_id=False))
     print(f"Successfully set ODrive {node_id} to 0 [Nm]")
