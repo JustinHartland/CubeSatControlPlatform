e4abea1cf171b3fe51a85e3b09f8fa4c066e021f
Justin Hartland <justinhartland2@gmail.com>
2023-11-06 15:27:03 -0500

    Treated running as threading event

--- invertedPendulumV2/InvertedPendulumPID.py
+++ invertedPendulumV2/InvertedPendulumPID.py
@@ -20,7 +20,7 @@ class InvertedPendulumPID:
 
     #Thread to set motor velocity, CHANGE TO TORQUE CONTROL
     def set_vel_thread(self, imu_obj, node_id, bus, running):
-        while running:
+        while running.is_set():
             velocity = self.pid(imu_obj.angle_x)
             bus.send(can.Message(arbitration_id=(node_id << 5 | 0x0d), data=struct.pack('<ff', float(velocity), 0.0), is_extended_id=False))
             
@@ -28,7 +28,7 @@ class InvertedPendulumPID:
 
     # Function to set torque for a specific O-Drive
     def set_torque_thread(self, imu_obj, node_id, bus, running):
-        while running:
+        while running.is_set():
             torque = self.pid(imu_obj.angle_x)
             bus.send(can.Message(
                 arbitration_id=(node_id << 5 | 0x0E),  # 0x0E: Set_Input_Torque
@@ -41,7 +41,7 @@ class InvertedPendulumPID:
 
        # Function to set torque to 0 for a specific O-Drive
     def set_torque_0(self, node_id, bus, running):
-        while running:
+        while running.is_set():
             
             bus.send(can.Message(
                 arbitration_id=(node_id << 5 | 0x0E),  # 0x0E: Set_Input_Torque
@@ -54,21 +54,21 @@ class InvertedPendulumPID:
 
     #Thread to read in orientation angle from IMU
     def read_angle_thread(self, imu_obj, running):
-        while running:
+        while running.is_set():
             imu_obj.get_euler_angles()
             time.sleep(0.001)
             
 
     #Prints arm angle and motor velocity
     def get_pos_vel_thread(self, imu_obj, node_id, bus, running):
-        while running:
+        while running.is_set():
             for msg in bus:
                 if msg.arbitration_id == (node_id << 5 | 0x09):
                     pos, vel = struct.unpack('<ff', bytes(msg.data))
                     print(f"Roll: {imu_obj.angle_x:.2f} degrees, vel: {vel:.3f} [turns/s]")
 
     def add_data_to_database(self, imu_obj, db, initial_time, trial_id, running):
-        while running:
+        while running.is_set():
             imuData = [time.time()-initial_time, *imu_obj.rawAccelArray, *imu_obj.rawGyroArray, imu_obj.angle_x, imu_obj.angle_y, imu_obj.angle_z]
             db.add_imu_data(trial_id, imuData)
             time.sleep(0.001)
