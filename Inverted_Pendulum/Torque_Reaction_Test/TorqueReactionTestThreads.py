8e6990cf675e484e751c28e58dae4db8ee521bde
Justin Hartland <justinhartland2@gmail.com>
2023-12-01 16:52:31 -0500

    debug

--- Inverted_Pendulum/Torque_Reaction_Test/TorqueReactionTestThreads.py
+++ Inverted_Pendulum/Torque_Reaction_Test/TorqueReactionTestThreads.py
@@ -48,19 +48,17 @@ class TorqueReactionTestThreads:
             bus.send(msg)
 
             message = bus.recv()  # Blocking call
-            if message.arbitration_id == (node_id << 5 | 0x1C):  # Replace with the correct response ID
+            #if message.arbitration_id == (node_id << 5 | 0x1C):  # Replace with the correct response ID
 
                 # Parse the data to get encoder estimates
-                torque_setpoint, torque_estimate = struct.unpack('<ff', message.data)
+            torque_setpoint, torque_estimate = struct.unpack('<ff', message.data)
 
-                self.torque_setpoint = torque_setpoint
-                self.torque_estimate = torque_estimate
+            self.torque_setpoint = torque_setpoint
+            self.torque_estimate = torque_estimate
 
-                print(self.torque_setpoint)
+            print(self.torque_setpoint)
 
             time.sleep(0.001) 
-
-            print('Bad News Bears')
 
 
     def add_data_to_database(self, db_path, initial_time, trial_id, running):
