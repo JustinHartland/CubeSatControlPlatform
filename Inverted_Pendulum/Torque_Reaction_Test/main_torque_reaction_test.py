e60052b67fe47b13792965d544e88b682ca87866
Justin Hartland <justinhartland2@gmail.com>
2023-12-03 17:43:27 -0500

    fixed err

--- Inverted_Pendulum/Torque_Reaction_Test/main_torque_reaction_test.py
+++ Inverted_Pendulum/Torque_Reaction_Test/main_torque_reaction_test.py
@@ -75,7 +75,7 @@ finally:
     print(f"Successfully set ODrive {node_id} to 0 [Nm]")
 
     for i in range(len(TorqueReactionTestThreads.time)):
-        torque_reaction_test_database.add_data(trial_id, (TorqueReactionTestThreads.time[i], TorqueReactionTestThreads.torque_setpoint[i], TorqueReactionTestThreads.torque_estimate[i]))
+        torque_reaction_test_database.add_data(trial_id, (TorqueReactionTestThreads.time_array[i], TorqueReactionTestThreads.torque_setpoint_array[i], TorqueReactionTestThreads.torque_estimate_array[i]))
 
     # Shutdown the bus in the finally block to ensure it's always executed
     bus.shutdown()
