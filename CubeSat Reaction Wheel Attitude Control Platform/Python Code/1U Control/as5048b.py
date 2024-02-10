aee54c9838c9dacbcfe65edcdc979a726d23bacd
Justin Hartland <justinhartland2@gmail.com>
2024-02-10 16:30:53 -0500

    thsg

--- CubeSat Reaction Wheel Attitude Control Platform/Python Code/1U Control/as5048b.py
+++ CubeSat Reaction Wheel Attitude Control Platform/Python Code/1U Control/as5048b.py
@@ -6,18 +6,10 @@ import time
 class as5048b:
     def __init__(self):
         self.bus = smbus.SMBus(1)
-
-        # AS5048A default address
-        self.AS5048A_ADDR = 0x40
-
-        # AS5048A Register
-        self.AS5048A_ANGLE_REG = 0xFE
-
-        #Sensor angle
-        self.angle = 0
-
-        #Offset angle to calibrate
-        self.offset_angle = 0
+        self.AS5048B_ADDR = 0x40 # AS5048B default address
+        self.AS5048B_ANGLE_REG = 0xFE # AS5048B Register
+        self.angle = 0 #Sensor angle
+        self.offset_angle = 0 #Offset angle to calibrate
 
     def calibrate_encoder(self):
         print('Ensure CoM is below pivot\n\nCalibration beginning in:\n\n')
@@ -29,39 +21,43 @@ class as5048b:
             time.sleep(1)
 
         #Determine offset
-        data = self.bus.read_i2c_block_data(self.AS5048A_ADDR, self.AS5048A_ANGLE_REG, 2)
+        data = self.bus.read_i2c_block_data(self.AS5048B_ADDR, self.AS5048B_ANGLE_REG, 2)
         angle_pre_conversion = data[0] * 256 + data[1]
         resting_angle = (angle_pre_conversion / 16383.0) * 45
 
-        print(resting_angle)
+        print(f"Resting angle: {resting_angle} degrees")
+        self.offset_angle = resting_angle  # Here we simply store the resting angle as the offset
 
-        self.offset_angle = 180 - resting_angle
-
-        print(self.offset_angle)
-
-        time.sleep(10)
+        print(f"Offset angle set to: {self.offset_angle} degrees")
 
 
     def read_angle_synch(self):
         '''
-        Read angular position of motor shaft
+        Read angular position of motor shaft and adjust with offset angle
         
-        return: angular position of motor shaft
+        return: return: Adjusted angular position of motor shaft
         '''
 
         # Read data from the angle register
-        data = self.bus.read_i2c_block_data(self.AS5048A_ADDR, self.AS5048A_ANGLE_REG, 2)
+        data = self.bus.read_i2c_block_data(self.AS5048B_ADDR, self.AS5048B_ANGLE_REG, 2)
 
         # Convert the data
         angle_pre_conversion = data[0] * 256 + data[1]
+        self.angle = (angle_pre_conversion / 16383.0) * 45.0
 
-        # Full range of the sensor is 0 to 16383
-        # Convert to degrees (0 to 360)
-        self.angle = (angle_pre_conversion / 16383.0) * 45.0 - self.offset_angle
+        # Adjust angle with the offset
+        self.angle = self.angle - self.offset_angle
 
         print(self.angle)
 
         time.sleep(0.001)
+
+## Example code
+encoder = as5048b()
+encoder.calibrate_encoder()
+
+while (True):
+    encoder.read_angle_synch()
 
     def read_angle_threading(self, running):
         '''
@@ -83,10 +79,3 @@ class as5048b:
             print(self.angle)
 
             time.sleep(0.001)
-
-## Example code
-encoder = as5048b()
-encoder.calibrate_encoder()
-
-while (True):
-    encoder.read_angle_synch()
