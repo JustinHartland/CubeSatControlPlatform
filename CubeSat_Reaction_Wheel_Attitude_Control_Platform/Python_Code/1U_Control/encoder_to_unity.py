b1dafbe65409ac3063d734c1318bf54833a71092
Justin Hartland <justinhartland2@gmail.com>
2024-04-09 18:33:47 -0400

    update

--- CubeSat_Reaction_Wheel_Attitude_Control_Platform/Python_Code/1U_Control/encoder_to_unity.py
+++ CubeSat_Reaction_Wheel_Attitude_Control_Platform/Python_Code/1U_Control/encoder_to_unity.py
@@ -27,7 +27,7 @@ try:
         angle = encoder.read_angle()
         print(angle)
 
-        data = (0, 0, angle)
+        data = "0, 0, " + angle
         print(data)
 
         datastr = str(data)
